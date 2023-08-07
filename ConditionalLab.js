98113a672f9ab89e44b7477e4356aa40734b1951
yian saechao <yian.l.saechao@gmail.com>
2023-08-06 20:19:57 -0700

    2nd commit

---
+++ ConditionalLab.js
@@ -0,0 +1,45 @@
+//1
+let numberOfStudents = 17;
+
+if (numberOfStudents > 20) {
+  console.log("classroom too small")
+}
+else console.log("classroom size acceptable")
+
+
+//2
+let numberOfHouses = 5;
+
+if (numberOfHouses > 2) {
+  console.log("you need to sell more houses")
+}
+if (numberOfHouses <= 2) {
+  console.log("seems reasonable")
+}
+
+
+//3
+let bill = 20;
+
+if (bill > 15) {
+  console.log("we should split the bill")
+}
+else console.log("I’ll get the bill this time")
+
+
+//4
+let studentScore = 90;
+
+if (studentScore <= 55) {
+    console.log("Not too good");
+} else if (studentScore <= 60) {
+    console.log("ehhhh");
+} else if (studentScore <= 70) {
+    console.log("Keep trying");
+} else if (studentScore <= 80) {
+    console.log("nice!");
+} else if (studentScore <= 90) {
+    console.log("Doing good!");
+} else {
+    console.log("over 90! great!");
+}
